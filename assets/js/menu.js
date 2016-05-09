bce844d9418466453040b51c348096472ffa1b93
Cristina <cristinaboixcanut@gmail.com>
2016-05-09 23:54:26 +0200

    update js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,2 +1,48 @@
 $(document).ready(function(){
-});
+
+
+// DESKTOP - Submenu Desplegado //
+
+	$('#menu-container li').hover(function(e) {
+		$(this).find('.sub-menu').slideToggle();
+	});
+
+
+// DESKTOP - Item Menu Seleccionado //
+	
+	$('#menu-container a').click(function(e) {
+		$('#menu-container li').removeClass('current-menu-item');
+		$(this).parent().addClass('current-menu-item');
+	});
+
+
+// MOBILE - Icono Menu Hamburguesa
+
+	$('#mobile-button').click(function() {
+		var menu = $('#menu-container');  
+		menu.show();
+		$('#mobile-close').show();
+		$('#mobile-button').hide();
+	});
+
+	$('#mobile-close').click(function() {
+		var menu = $('#menu-container');  
+		menu.hide();
+		$('#mobile-close').hide();
+		$('#mobile-button').show();
+	});
+
+
+	$(window).resize(function() {
+	
+		if($(window).width() > 991) {
+			$('#mobile-close').hide();
+			$('#mobile-button').hide();
+	
+		}else{
+			$('#mobile-button').show();
+		}
+
+	});
+
+ });
